02b0b4f9e87d1c4c84c51a0f827182f3ebd40cea
mohankumarn24 <mohankumar.n24@outlook.com>
2025-11-25 16:58:01 +0530

    add notes

--- src/main/java/net/projectsync/actuators/config/EmployeeProperties.java
+++ src/main/java/net/projectsync/actuators/config/EmployeeProperties.java
@@ -8,44 +8,29 @@ import org.springframework.context.annotation.Configuration;
 public class EmployeeProperties {
 	
 	private String id;
-	
 	private String firstname;
-	
 	private String lastname;
-	
 	private String address;
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
+	// getters, setters
+	public String getId() { return id; }
+	public String getFirstname() { return firstname; }
+	public String getLastname() { return lastname; }
+	public String getAddress() { return address; }
+	
+	public void setId(String id) { this.id = id; }
+	public void setFirstname(String firstname) { this.firstname = firstname; }
+	public void setLastname(String lastname) { this.lastname = lastname; }
+	public void setAddress(String address) { this.address = address; }
+	
+	/*	
+	Spring Boot @ConfigurationProperties Case Sensitivity Rules:
+	 - Spring matches the property names to fields by converting your field names to relaxed binding names.
+	 - Spring Boot uses Relaxed Binding, which means the following are all equivalent
+		 test.employee.firstname
+		 test.employee.first-name
+		 test.employee.first_name
+		 test.employee.FIRSTNAME
+		 test.employee.FirstName
+	 */
 }
-
